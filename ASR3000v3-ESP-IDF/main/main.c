36ebb5323f9713d37c018b95988c0095d85c44b3
Bruno Melão <114967953+brunomelao@users.noreply.github.com>
2024-09-09 10:50:28 -0300

    Configurações iniciais do projeto

---
+++ ASR3000v3-ESP-IDF/main/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void app_main(void)
+{
+
+}
+
+/* A fazer no código:
+- Conversão ADC tensão na bateria
+- Salvamento de dados no LittleFS
+- Salvamento de dados no SD
+- Transmissão pelo LoRa com interrupção
+- Aquisição de dados do GPS
+- Aquisição de dados do acelerômetro/giroscópio/magnetômetro
+- Aquisição de dados do barômetro
+- Filas dos dados
+- Lógica de acionamento 
+- Bitwise para Status
+*/
